13417a842a0ba3e590b7a233c15d9b361e2a68a0
Swet <spancha2@uncc.edu>
2021-10-18 11:37:08 -0400

    Added initial Flask App

---
+++ index.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/index')
+def index():
+  return render_template('index.html')
+
+if __name__ == "__main__":
+  app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
+

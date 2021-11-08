41574f44d10e196a539e5dab9c71b99951de0822
Swet <spancha2@uncc.edu>
2021-11-08 11:10:00 -0500

    Fixed everything for templates and connection for intial function

--- index.py
+++ index.py
@@ -4,9 +4,41 @@ from flask import render_template
 
 app = Flask(__name__)
 
+@app.route('/')
 @app.route('/index')
 def index():
   return render_template('index.html')
+
+
+@app.route('/about')
+def aboutUs():
+  return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+  return render_template('contact.html')
+
+@app.route('/addPost')
+def addPost():
+  return render_template('addPost.html')
+
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
+@app.route('/signUp')
+def sign_up():
+  return render_template('signup.html')
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
   app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)

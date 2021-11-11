bd2464bc8b8e00ac2ab4e37dea29bef3eeea62de
Swet <spancha2@uncc.edu>
2021-11-10 21:35:07 -0500

    Whole Baseline App ready(without css)

--- models.py
+++ models.py
@@ -7,16 +7,15 @@ class Post(db.Model):
     title = db.Column("title", db.String(200))
     text = db.Column("text", db.String(100))
     date = db.Column("date", db.String(50))
-    name = db.Column("name", db.String(100))
-    # comments = db.relationship("Comment", backref="user", cascade="all, delete-orphan", lazy=True)
-    # can create a foreign key; referencing the id variable in the User class, so that is why it is lowercase u
+    # can create a foreign key: referencing the id variable in the User class,
+    # so that is why it is lowerase u
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    comments = db.relationship("Comment", backref="post", cascade="all, delete-orphan", lazy=True)
 
-    def __init__(self, title, text, date, name, user_id):
+    def __init__(self, title, text, date, user_id):
         self.title = title
         self.text = text
         self.date = date
-        self.name = name
         self.user_id = user_id
 
 
@@ -25,12 +24,28 @@ class User(db.Model):
     first_name = db.Column("first_name", db.String(100))
     last_name = db.Column("last_name", db.String(100))
     email = db.Column("email", db.String(100))
+    password = db.Column(db.String(255), nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
+    posts = db.relationship("Post", backref="user", lazy=True)
+    comments = db.relationship("Comment", backref="user", lazy=True)
 
-    # password = db.Column(db.String(255), nullable=False)
-    # registered_on = db.Column(db.DateTime, nullable=False)
-    # notes = db.relationship("Note", backref="user", lazy=True)
-    # comments = db.relationship("Comment", backref="user", lazy=True)
-
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, email, password):
         self.first_name = first_name
         self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.registered_on = datetime.date.today()
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_posted = db.Column(db.DateTime, nullable=False)
+    content = db.Column(db.VARCHAR, nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __init__(self, content, post_id, user_id):
+        self.date_posted = datetime.date.today()
+        self.content = content
+        self.post_id = post_id
+        self.user_id = user_id

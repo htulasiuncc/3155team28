6d1bcab00dcb251eeea56ad5db534f4bfa131b0c
Himanshu Makadiya <hmakadiy@uncc.edu>
2021-11-11 22:59:13 -0500

    Modified HTML and CSS files for the project

--- index.py
+++ index.py
@@ -45,6 +45,14 @@ def index():
         return render_template('index.html', user=session['user'])
     return render_template("index.html")
 
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 
 @app.route('/viewPosts')
 def get_posts():
